19060f613ed74c3afbf536c0542703cea599cedf
Markus Klemm <markus@markus@markusklemm.net>
2016-12-10 16:40:28 +0100

    Initial

---
+++ cmake-build-debug/CMakeFiles/CPP_Spielwiese.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Spielwiese.dir/main.cpp.o"
+  "CPP_Spielwiese.pdb"
+  "CPP_Spielwiese"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Spielwiese.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CPP_Spielwiese.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPP_Spielwiese.
+# This may be replaced when dependencies are built.
