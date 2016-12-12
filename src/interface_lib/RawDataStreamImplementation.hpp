1e1de35efdd9dba8bc1372aa286340e9e2b23328
Markus Klemm <markus@markus@markusklemm.net>
2016-12-12 16:24:31 +0100

    May implement bitshift myself

--- src/interface_lib/RawDataStreamImplementation.hpp
+++ src/interface_lib/RawDataStreamImplementation.hpp
@@ -9,6 +9,11 @@
 
 #include <memory>
 #include <istream>
+#include <array>
+#include <cstdint>
+#include <algorithm>
+#include <deque>
+#include <bitset>
 
 class RawDataStreamImplementation : public RawDataStream {
 public:
@@ -17,11 +22,46 @@ public:
         no_data = -1,
     };
 
+    static constexpr unsigned bit_align_mark_length = 5;
+    static constexpr char bit_align_mark[bit_align_mark_length] = {0x00, 0x00, 0x00, 0x00, (char) (0x80)};
+    static constexpr unsigned read_block_size = 7; //Prime so the probability is low to always read in the middle of bit align marks
+
     virtual int readBytes(unsigned char *buffer, int num_bytes) override {
-        std::char_traits<char>::int_type byte = this->byte_stream_->get();
-        if (byte == std::char_traits<char>::eof()) {
+        if (num_bytes <= 0) {
+            return 0;
+        }
+
+        if (num_bytes <= cached_.size()) {
+            for (size_t i = 0; i < num_bytes; ++i) {
+                buffer[i] = cached_.front();
+            }
+            return num_bytes;
+        }
+
+        std::array<char, read_block_size> read_buffer = {};
+        byte_stream_->read(read_buffer.data(), read_block_size);
+        const auto read_count = byte_stream_->gcount();
+
+        if (read_count == 0 && byte_stream_->eof()) {
             return static_cast<int> (readBytes_return_code::end_of_stream);
         }
+
+        auto check_for_misalign = [](const std::array<char, read_block_size> &read_buffer) -> unsigned {
+            //bit shift range from 0 in both -/+infty
+            //Check for mark
+            std::array<char, read_block_size + 2> check_buffer = {};
+            memcpy(check_buffer.data() + 1, read_buffer.data(), read_buffer.size());
+
+            /*! @param shift_by 8 < Value < 8
+             *
+             */
+            auto shift_multi_byte = [&](int shift_by) {
+                char *first = check_buffer.data() + 1;
+                if (shift_by > 0) {
+
+                }
+            };
+        };
 
 
         return static_cast<int> (readBytes_return_code::no_data);
@@ -30,8 +70,9 @@ public:
     RawDataStreamImplementation(std::unique_ptr<std::istream> &&stream)
             : RawDataStream(), byte_stream_(std::move(stream)) {}
 
-protected:
+private:
     std::unique_ptr<std::istream> byte_stream_;
+    std::deque<char> cached_;
 };
 
 
