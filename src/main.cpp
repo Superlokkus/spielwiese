908fe9e51facc33830b29173c02d0481008c530d
Markus Klemm <markus.klemm@watttron.com>
2020-07-20 12:59:08 +0200

    Works

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-#include <lib_name/lib_name.hpp>
+#include <boost/python.hpp>
 
-int main() {
-    lib_name::lib a;
-    std::cout << a.foo() << std::endl;
-    return 0;
+char const* greet(unsigned x)
+{
+    static char const* const msgs[] = { "hello", "Boost.Python", "world!" };
+
+    if (x > 2)
+        throw std::range_error("greet: index out of range");
+
+    return msgs[x];
+}
+
+BOOST_PYTHON_MODULE(CPP_Spielwiese)
+{
+    boost::python::def("greet", greet, "return one of 3 parts of a greeting");
 }
