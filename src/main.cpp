138be901b75580ea8c34d937fcc1c4ecf252b275
Markus Klemm <markus@markus@markusklemm.net>
2022-02-13 21:31:51 +0100

    ssl stil has probs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
-#include <lib_name/lib_name.hpp>
+#include <SDL.h>
+#include <SDL_audio.h>
+
 
 int main() {
-    lib_name::lib a;
-    std::cout << a.foo() << std::endl;
-    return 0;
+    SDL_Init(SDL_INIT_AUDIO);
+
+    SDL_AudioSpec spec{};
+    Uint8 *audio_buf{};
+    Uint32 audio_len{};
+
+    SDL_LoadWAV("/Users/markus/Downloads/CantinaBand60.wav", &spec, &audio_buf, &audio_len);
+
+    SDL_AudioSpec ob_spec{};
+    auto device = SDL_OpenAudioDevice(NULL, 0, &spec, &ob_spec, SDL_AUDIO_ALLOW_ANY_CHANGE);
+
+    SDL_QueueAudio(device, audio_buf, audio_len);
+    SDL_PauseAudioDevice(device, 0);
+
+    SDL_Delay(5000);
+
+    SDL_CloseAudioDevice(device);
+    SDL_FreeWAV(audio_buf);
+
+    SDL_QuitSubSystem(SDL_INIT_AUDIO);
 }
